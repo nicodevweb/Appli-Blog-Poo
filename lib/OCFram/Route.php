22de493f974882c21fbc045ad08a7c327ad3bb5c
Nicolas <clad87ster@gmail.com>
2017-06-07 21:02:40 +0200

    Add classes Route.php and Router.php in OCFram folder

---
+++ lib/OCFram/Route.php
@@ -0,0 +1,102 @@
+<?php
+// Classe traitant des routes : une URL associée à un module et une action
+
+namespace OCRFram;
+
+class Route
+{
+	protected	$action,
+				$module,
+				$url,
+				$varsNames,
+				$vars = [];
+
+	// Le constructeur s'occupe d'hydrater les attributs de son instance
+	public function __construct($url, $module, $action, array $varsNames)
+	{
+		$this->setUrl($url);
+		$this->setModule($module);
+		$this->setAction($action);
+		$this->setVarsNames($varsNames);
+	}
+
+	// Méthode vérfiant l'existance des variables
+	public function hasVars()
+	{
+		return !empty($this->varsNames);
+	}
+
+	// Méthode permettant de retourner l'existance de l'url entrée dans la liste des urls de l'application
+	public function match($url)
+	{
+		if (preg_match('`^' . $this->url . '$`', $url, $matches))
+		{
+			return $matches;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	// Setters de la classe Route
+	public function setAction($action)
+	{
+		if (is_string($action))
+		{
+			$this->action = $action;
+		}
+	}
+
+	public function setModule($module)
+	{
+		if (is_string($module))
+		{
+			$this->module = $module;
+		}
+	}
+
+	public function setUrl($url)
+	{
+		if (is_string($url))
+		{
+			$this->url = $url;
+		}
+	}
+
+	public function setVarsNames(array $varsNames)
+	{
+		$this->varsNames = $varsNames;
+	}
+
+	public function setVars(array $vars)
+	{
+		$this->vars = $vars;
+	}
+
+	// Getters de la classe Route
+	public function action()
+	{
+		return $this->action;
+	}
+
+	public function module()
+	{
+		return $this->module;
+	}
+
+	public function url()
+	{
+		return $this->url;
+	}
+
+	public function varsNames()
+	{
+		return $this->varsNames;
+	}
+
+	public function vars()
+	{
+		return $this->vars;
+	}
+}

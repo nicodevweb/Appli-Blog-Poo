c6f3598799b59dcf52ec1e8a63f6f8f75a55b170
Nicolas <clad87ster@gmail.com>
2017-06-09 16:15:08 +0200

    Update BackController.php to pair with setContentFile() form class Page.php

--- lib/OCFram/BackController.php
+++ lib/OCFram/BackController.php
@@ -79,5 +79,7 @@ abstract class BackController extends ApplicationComponent
 		}
 
 		$this->view = $view;
+
+		$this->page->setContentFile(__DIR__.'/../../App/'.$this->app->name().'/Modules/'.$this->module.'/Views/'.$this->view.'.php');
 	}
 }

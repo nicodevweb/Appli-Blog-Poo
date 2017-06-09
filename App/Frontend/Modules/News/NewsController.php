3b6ce7776a25c2fa3117cbc296bddf2c51397dc4
Nicolas <clad87ster@gmail.com>
2017-06-09 23:28:21 +0200

    NewsController coded, add config accordingly in app.xml

---
+++ App/Frontend/Modules/News/NewsController.php
@@ -0,0 +1,43 @@
+<?php
+// Classe permettant :
+// 1- De renvoyer les 5 dernières news (à configurer dans App/Frontend/Config/app.xml)
+// 2- Parcourir cette liste de news et n'assigner au news qu'un contenu de 200 caractères max
+// 3- Passer la liste de news à la vue
+
+namespace App\Frontend\Modules\News;
+
+use \OCFram\BackController;
+use \OCFram\HTTPRequest;
+
+class NewsController extends BackController
+{
+  public function executeIndex(HTTPRequest $request)
+  {
+    $nombreNews = $this->app->config()->get('nombre_news');
+    $nombreCaracteres = $this->app->config()->get('nombre_caracteres');
+    
+    // On ajoute une définition pour le titre.
+    $this->page->addVar('title', 'Liste des '.$nombreNews.' dernières news');
+    
+    // On récupère le manager des news.
+    $manager = $this->managers->getManagerOf('News');
+    
+    // Cette ligne, vous ne pouviez pas la deviner sachant qu'on n'a pas encore touché au modèle.
+    // Contentez-vous donc d'écrire cette instruction, nous implémenterons la méthode ensuite.
+    $listeNews = $manager->getList(0, $nombreNews);
+    
+    foreach ($listeNews as $news)
+    {
+      if (strlen($news->contenu()) > $nombreCaracteres)
+      {
+        $debut = substr($news->contenu(), 0, $nombreCaracteres);
+        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+        
+        $news->setContenu($debut);
+      }
+    }
+    
+    // On ajoute la variable $listeNews à la vue.
+    $this->page->addVar('listeNews', $listeNews);
+  }
+}

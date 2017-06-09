9ed212bbc41668d3cd7d077af290607fea346550
Nicolas <clad87ster@gmail.com>
2017-06-09 18:48:28 +0200

    Add 404.html page and Application FrontendApplication.php in App/Frontend

---
+++ App/Frontend/FrontendApplication.php
@@ -0,0 +1,26 @@
+<?php
+// Obtention du contrôleur grâce à la méthode parente getController().
+// Exécution du contrôleur.
+// Assignation de la page créée par le contrôleur à la réponse.
+// Envoi de la réponse.
+
+
+
+class FrontendApplication extends Application
+{
+  public function __construct()
+  {
+    parent::__construct();
+
+    $this->name = 'Frontend';
+  }
+
+  public function run()
+  {
+    $controller = $this->getController();
+    $controller->execute();
+
+    $this->httpResponse->setPage($controller->page());
+    $this->httpResponse->send();
+  }
+}
